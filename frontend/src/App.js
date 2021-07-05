766574e85a24b38dedf8c298362eac9266fed72e
matthew-holmes <MHolmes1@qa.com>
2021-07-05 15:29:14 +0100

    MH: Add basic footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './Components/Navbar/Nav';
-import Home from './Components/Home/Home'
+import Home from './Components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 import './Resources/App.css';
+import { Component } from 'react';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
           </Route> */}
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <div class='container'>
+            <div class='row'>
+                <div class='col-sm'>
+                    <p href='/AboutUs'> About Us </p>
+                </div>
+                <div class='col-sm'>
+                    <p href='/AboutUs'> Contact Us </p>
+                </div>
+            </div>
+            <div class='col'>
+                <p> QA Cinema 2021 &copy; </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
