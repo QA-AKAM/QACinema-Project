ee6c35bbbdd1f70409435620262eba7aaccd7df7
matthew-holmes <MHolmes1@qa.com>
2021-07-05 16:33:31 +0100

    MH: Small formatting change to table

--- frontend/src/Components/Information/OpeningTimes.jsx
+++ frontend/src/Components/Information/OpeningTimes.jsx
@@ -7,6 +7,7 @@ const OpeningTimes = () => {
                     <tr>
                         <th scope="col">Day of the Week</th>
                         <th scope="col">Opening</th>
+                        <th scope="col"></th>
                         <th scope="col">Closing</th>
                     </tr>
                 </thead>
@@ -14,36 +15,43 @@ const OpeningTimes = () => {
                     <tr>
                         <th scope="row">Monday</th>
                         <td>13:00</td>
+                        <td> - </td>
                         <td>22:00</td>
                     </tr>
                     <tr>
                         <th scope="row">Tuesday</th>
                         <td>13:00</td>
+                        <td> - </td>
                         <td>22:00</td>
                     </tr>
                     <tr>
                         <th scope="row">Wednesday</th>
                         <td>13:00</td>
+                        <td> - </td>
                         <td>22:00</td>
                     </tr>
                     <tr>
                         <th scope="row">Thursday</th>
                         <td>13:00</td>
+                        <td> - </td>
                         <td>22:00</td>
                     </tr>
                     <tr>
                         <th scope="row">Friday</th>
                         <td>13:00</td>
+                        <td> - </td>
                         <td>23:30</td>
                     </tr>
                     <tr>
                         <th scope="row">Saturday</th>
                         <td>9:30</td>
+                        <td> - </td>
                         <td>23:30</td>
                     </tr>
                     <tr>
                         <th scope="row">Sunday</th>
                         <td>9:30</td>
+                        <td> - </td>
                         <td>23:30</td>
                     </tr>
                 </tbody>
