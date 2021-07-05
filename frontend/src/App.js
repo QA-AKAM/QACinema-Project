6504e764829db1b798efc3e82ff441ff4e5a4147
Kevin <kdoan@qa.com>
2021-07-05 15:08:07 +0100

    #CP-8 KD: Finished NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './Resources/logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './Components/Navbar/Nav';
+import Home from './Components/Home/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './Resources/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+
+          {/* <Route path="/Home" >
+            <Home />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
