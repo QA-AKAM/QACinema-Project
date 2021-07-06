492a4cf2e12e7973b83a73b532b6601f477a9f91
matthew-holmes <MHolmes1@qa.com>
2021-07-06 17:11:06 +0100

    MH: Background/Format Fix

--- frontend/src/Components/Information/Screens.jsx
+++ frontend/src/Components/Information/Screens.jsx
@@ -1,66 +1,69 @@
+import '../../CSS/Pages.css';
 import { Card, Accordion, Button } from "react-bootstrap"
 
 const Screens = () => {
     return (
-        <div class='container'>
-            <h2> Screens </h2>
-            <Card>
-                <div class='row'>
-                    <div class='col-sm'>
-                        <img src="./Images/StandardDecor.png" alt='Our Standard Cinema Screen Decor' width='500px' />
+        <div>
+            <div class='background'>
+                <h1 class='landing-text'> Screens </h1>
+                <Card>
+                    <div class='row'>
+                        <div class='col-sm'>
+                            <img src="./Images/StandardDecor.png" alt='Our Standard Cinema Screen Decor' width='500px' />
+                        </div>
+                        <div class='col-sm'>
+                            <h4> Standard Seating </h4>
+                            <p> Look! Our seating is super bootiful! </p>
+                        </div>
                     </div>
-                    <div class='col-sm'>
-                        <h4> Standard Seating </h4>
-                        <p> Look! Our seating is super bootiful! </p>
+                    <div class='container'>
+                        <Accordion>
+                            <Card>
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant="text" eventKey="0">
+                                        View Seating Plan
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>
+                                        <img src="./Images/StandardPlan.png" alt='Standard Seating Plan' width='100%' />
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
                     </div>
-                </div>
-                <div class='container'>
-                    <Accordion>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="text" eventKey="0">
-                                    View Seating Plan
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>
-                                    <img src="./Images/StandardPlan.png" alt='Standard Seating Plan' width='100%' />
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-                </div>
+                    <p />
+                </Card>
                 <p />
-            </Card>
-            <p />
-            <Card>
-                <div class='row'>
-                    <div class='col-sm'>
-                        <h4> Deluxe Seating </h4>
-                        <p> Look! Our seating is super bootiful! </p>
+                <Card>
+                    <div class='row'>
+                        <div class='col-sm'>
+                            <h4> Deluxe Seating </h4>
+                            <p> Look! Our seating is super bootiful! </p>
+                        </div>
+                        <div class='col-sm'>
+                            <img src="./Images/DeluxeDecor.png" alt='Our Deluxe Cinema Screen Decor' width='500px' />
+                        </div>
                     </div>
-                    <div class='col-sm'>
-                        <img src="./Images/DeluxeDecor.png" alt='Our Deluxe Cinema Screen Decor' width='500px' />
+                    <div class='container'>
+                        <Accordion>
+                            <Card>
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant="text" eventKey="0">
+                                        View Seating Plan
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>
+                                        <img src="./Images/DeluxePlan.png" alt='Deluxe Seating Plan' width='100%' />
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
                     </div>
-                </div>
-                <div class='container'>
-                    <Accordion>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="text" eventKey="0">
-                                    View Seating Plan
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>
-                                    <img src="./Images/DeluxePlan.png" alt='Deluxe Seating Plan' width='100%' />
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-                </div>
-                <p />
-            </Card>
+                    <p />
+                </Card>
+            </div>
         </div>
     )
 }
