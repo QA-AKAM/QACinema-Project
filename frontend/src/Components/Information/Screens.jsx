1c563be40124a7b9695234e48b1eab15db4d3e77
matthew-holmes <MHolmes1@qa.com>
2021-07-06 10:59:54 +0100

    MH: format w/ Screens.css

--- frontend/src/Components/Information/Screens.jsx
+++ frontend/src/Components/Information/Screens.jsx
@@ -7,7 +7,7 @@ const Screens = () => {
             <Card>
                 <div class='row'>
                     <div class='col-sm'>
-                        <img src="../../Resources/Images/StandardDecor.png" alt='Our Standard Cinema Screen Decor' width='200px' />
+                        <img src="./Images/StandardDecor.png" alt='Our Standard Cinema Screen Decor' width='500px' />
                     </div>
                     <div class='col-sm'>
                         <h4> Standard Seating </h4>
@@ -24,7 +24,7 @@ const Screens = () => {
                             </Card.Header>
                             <Accordion.Collapse eventKey="0">
                                 <Card.Body>
-                                    <img src="../../Resources/Images/StandardPlan.png" alt='Standard Seating Plan' width='200px' />
+                                    <img src="./Images/StandardPlan.png" alt='Standard Seating Plan' width='100%' />
                                 </Card.Body>
                             </Accordion.Collapse>
                         </Card>
@@ -40,7 +40,7 @@ const Screens = () => {
                         <p> Look! Our seating is super bootiful! </p>
                     </div>
                     <div class='col-sm'>
-                        <img src="../../Resources/Images/DeluxeDecor.png" alt='Our Deluxe Cinema Screen Decor' width='200px' />
+                        <img src="./Images/DeluxeDecor.png" alt='Our Deluxe Cinema Screen Decor' width='500px' />
                     </div>
                 </div>
                 <div class='container'>
@@ -53,7 +53,7 @@ const Screens = () => {
                             </Card.Header>
                             <Accordion.Collapse eventKey="0">
                                 <Card.Body>
-                                    <img src="../../Resources/Images/DeluxePlan.png" alt='Deluxe Seating Plan' width='200px' />
+                                    <img src="./Images/DeluxePlan.png" alt='Deluxe Seating Plan' width='100%' />
                                 </Card.Body>
                             </Accordion.Collapse>
                         </Card>
