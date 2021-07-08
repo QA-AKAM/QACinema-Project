423108b8fdd0b1e772d0b624a7d35b07010c0ead
thealinivan <ivana@roehampton.ac.uk>
2021-07-08 15:01:15 +0100

    CP-38 Cp-29 AI: Updated components in directions

---
+++ frontend/src/Components/Information/Directions/DirectionsCard.jsx
@@ -0,0 +1,32 @@
+'use strict'
+
+const DirectionsCard = ({ cardData }) => {
+    return (
+        <div class="container" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', marginTop: '3rem' }}>
+            <h2 class='landing-text'>{cardData.title}</h2>
+            <h3 class='landing-text' style={{ color: '0,0,0', margin: '30px' }}>{cardData.description}</h3>
+            <a href={cardData.nav}
+                style={{
+                    border: '1px solid red',
+                    padding: '1rem',
+                    color: 'red'
+                }}>Directions</a>
+            <div class='container' style={{
+                backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                marginTop: '3rem',
+                overflow: 'hidden'
+            }}>
+                {cardData.img ?
+                    <img src={cardData.img}
+                        width="100%"
+                        alt="cinema front entrance"
+                        style={{ objectFit: 'cover' }} />
+                    : cardData.map
+                }
+            </div>
+        </div>
+    );
+}
+
+
+export default DirectionsCard;
