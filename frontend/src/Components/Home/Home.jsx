c1aab7ab1e6e364bc5349c5d8fbfdeb84f5a212a
Kevin <kdoan@qa.com>
2021-07-13 14:23:38 +0100

    KD:Responsive homepage.

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 'use strict';
-import { Carousel, Alert } from 'react-bootstrap';
+import { Carousel, Alert, Container, Col, Row } from 'react-bootstrap';
 import InTheHeights from '../../Images/In-The-Heights.jpg';
 import BlackWidow from '../../Images/Black-Widow.jpg';
 import F9 from '../../Images/Fast9.jpg';
@@ -11,7 +11,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div class="background">
-            <div class="carouselImg">
+            <Container>
                 <Carousel>
                     <Carousel.Item interval={8000}>
                         {/* <a href="/Movies">
@@ -53,24 +53,26 @@ const Home = () => {
                         />
                     </Carousel.Item>
                 </Carousel>
-            </div>
+            </Container>
             <div class="bgBlur">
                 <div id="home">
                     <h1 class='landing-text'> Welcome to QA Cinemas</h1>
                     <h3 class="landing-text">Experience all the latest movies in stunning IMAX!</h3>
                 </div>
                 <div class="container">
-                    <div class="row">
-                        <div class="spec col-sm-6 text-center">
+                    <Row>
+                        <Col class="spec col-lg-6 text-center">
                             <h2 class="display-4">Unrivaled Quality</h2>
                             <p class="lead">
                                 Immerse yourself completely in the big screen experience with QA Cinemas IMAX. Every aspect of our IMAX Screens is designed to pull you into the film. Indulge in the journey that tickles your senses and brings them back to life. Transporting yourself into a new reality with our super-sized screens pulling you into the film, ear-tingling audio environment to ensure you can even hear a pin drop and where within the film as well as breath-taking imagery, watching movies on your computer will never be the same again.
                             </p>
-                        </div>
-                        <div class="col-lg-6">
-                            <img src={IMAX} alt="Imax Logo" class="imgDiv" />
-                        </div>
-                    </div>
+                        </Col>
+                        <Col class="col-sm-6">
+                            <Container>
+                                <img src={IMAX} alt="Imax Logo" class="imgDiv" />
+                            </Container>
+                        </Col>
+                    </Row>
                     <div>
                         <img src={IMAXScreen} alt="Imax Screen img" class="imgContainer" />
                     </div>
