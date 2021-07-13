fae4e4b7e533c60790505eab5208160df416b103
Kevin <kdoan@qa.com>
2021-07-13 17:28:46 +0100

    KD: Refactoring and adding info on About page.

--- frontend/src/Components/Information/ContactUs.jsx
+++ frontend/src/Components/Information/ContactUs.jsx
@@ -43,54 +43,55 @@ const ContactUs = () => {
 
     return (
         <div class='background'>
-            <Container className="text-white">
-                <Jumbotron className="bgBlur">
+            <Jumbotron className="bgBlur">
+                <Container className="text-white">
                     <h1 class='landing-text'> Contact Us </h1>
+                    <Container className="center">
+                        <Form onSubmit={handleSubmit} noValidate validated={validated} style={{ flex: 1, backgroundColor: '#A02626' }} className="rounded">
+                            <Form.Group as={Row} className="mb-3">
+                                <Form.Label column sm="2" className="text-right pr-4 mt-3"> Your E-mail </Form.Label>
+                                <Form.Control required name="email" type='email' id='email' className="center mt-3" placeholder="name@example.com" onChange={(event) => {
+                                    setEmail(event.target.value);
+                                }} value={email}></Form.Control>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3 pr-4" >
+                                <Form.Label column sm="2" className="text-right"> Topic</Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control required as="select" name='subject' id='subject' className="center"
+                                        onChange={(event) => {
+                                            setTopic(event.target.value);
+                                        }} value={topic}>
+                                        <option value='feedback'> Feedback </option>
+                                        <option value='booking'> Venue Booking </option>
+                                        <option value='message'> Other </option>
+                                    </Form.Control>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3">
+                                <Form.Label column sm="2" className="text-right pr-4"> Your Message </Form.Label>
+                                <Form.Control required
+                                    name="message" as="textarea" id='message' placeholder="Leave a comment here" className="center"
+                                    onChange={(event) => {
+                                        setMessage(event.target.value);
+                                    }} value={message}></Form.Control>
+                            </Form.Group>
+                            <Button variant="outline-dark" id='submit' type='submit' size="lg"> Send </Button>
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title> Thanks for your E-mail!</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>We appreciate your {topic} and will get back you as soon as we can. You can expect a reply from us sent to {email} within the next 5 working days.</Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>
+                                        Close
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </Form>
+                    </Container>
+                </Container >
+            </Jumbotron>
 
-                <Form onSubmit={handleSubmit} noValidate validated={validated} style={{ flex: 1, backgroundColor: '#A02626' }} className="rounded">
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="2" className="text-right pr-4 mt-3"> Your E-mail </Form.Label>
-                        <Form.Control required name="email" type='email' id='email' className="mt-3" placeholder="name@example.com" onChange={(event) => {
-                            setEmail(event.target.value);
-                        }} value={email}></Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 pr-4" >
-                        <Form.Label column sm="2" className="text-right"> Topic</Form.Label>
-                        <Col sm="10">
-                            <Form.Control required as="select" name='subject' id='subject'
-                                onChange={(event) => {
-                                    setTopic(event.target.value);
-                                }} value={topic}>
-                                <option value='feedback'> Feedback </option>
-                                <option value='booking'> Venue Booking </option>
-                                <option value='message'> Other </option>
-                            </Form.Control>
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="2" className="text-right pr-4"> Your Message </Form.Label>
-                        <Form.Control required
-                            name="message" as="textarea" id='message' placeholder="Leave a comment here"
-                            onChange={(event) => {
-                                setMessage(event.target.value);
-                            }} value={message}></Form.Control>
-                    </Form.Group>
-                    <Button variant="outline-dark" id='submit' type='submit' size="lg"> Send </Button>
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title> Thanks for your E-mail!</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>We appreciate your {topic} and will get back you as soon as we can. You can expect a reply from us sent to {email} within the next 5 working days.</Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
-                </Form>
-                </Jumbotron>
-
-            </Container >
             <div id="fixed">
             </div>
         </div >
