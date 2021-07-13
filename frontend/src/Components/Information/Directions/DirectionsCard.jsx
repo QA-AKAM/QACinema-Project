15a64f6c56472be224a0ba6c7d4daf48e22f1771
thealinivan <ivana@roehampton.ac.uk>
2021-07-13 10:27:59 +0100

    CP-52 AI: Updated styling for directions

--- frontend/src/Components/Information/Directions/DirectionsCard.jsx
+++ frontend/src/Components/Information/Directions/DirectionsCard.jsx
@@ -1,21 +1,12 @@
-'use strict'
+import { Button } from 'react-bootstrap';
 
 const DirectionsCard = ({ cardData }) => {
     return (
-        <div class="container" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)', marginTop: '3rem', paddingBottom: '4rem' }}>
-            <h2 class='landing-text'>{cardData.title}</h2>
-            <h3 class='landing-text' style={{ color: '0,0,0', margin: '30px' }}>{cardData.description}</h3>
-            <a href={cardData.nav}
-                style={{
-                    border: '1px solid red',
-                    padding: '1rem',
-                    color: 'red'
-                }}>{cardData.btn}</a>
-            <div class='container' style={{
-                backgroundColor: 'rgba(0, 0, 0, 0.6)',
-                marginTop: '3rem',
-                overflow: 'hidden'
-            }}>
+        <div className="colorScheme marg-3 padd-3">
+            <h2 className='landing-text'>{cardData.title}</h2>
+            <h3 className='landing-text marg-3'>{cardData.description}</h3>
+            <Button className='main-btn marg-1 padd-3' variant="outline-dark" href={cardData.nav}>{cardData.btn}</Button>
+            <div style={{ overflow: 'hidden' }}>
                 {cardData.img ?
                     <img src={cardData.img}
                         width="100%"
@@ -24,7 +15,7 @@ const DirectionsCard = ({ cardData }) => {
                     : cardData.map
                 }
             </div>
-        </div>
+        </div >
     );
 }
 
