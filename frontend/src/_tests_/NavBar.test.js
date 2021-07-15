05cfab6bf1a49ce95a8dd4a40004320241afc9a2
ashkl <asshwin12@yahoo.co.uk>
2021-07-15 12:07:14 +0100

    CP-41 CP-61 AM: Attempt at testing the navbar. Coverage at 89.47%

--- frontend/src/_tests_/NavBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Nav from '../Components/Navbar/Nav';
-import renderer from 'react-test-renderer';
-
-test("It matches the snapshot", () => {
-    const tree = renderer.create(<Nav />).toJSON();
-    expect(tree).toMatchSnapshot();
-
-})
