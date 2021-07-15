933080fed20b4c1ac4bd8e77f4dfeb209e704e57
ashkl <asshwin12@yahoo.co.uk>
2021-07-15 12:02:18 +0100

    Revert "CP-41 CP-61 AM: Attempt at testing the navbar. Coverage at 89.47%"
    This reverts commit 619f79af85277d66e2ab5bbb7de856440ba9bac5.

--- frontend/src/_tests_/Nav/NavBar.test.js
+++ frontend/src/_tests_/Nav/NavBar.test.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import Nav from '../../Components/Navbar/Nav';
 import renderer from 'react-test-renderer';
+import { create } from 'react-test-renderer';
 import { BrowserRouter } from "react-router-dom";
 
 describe(`testing the navigation`, () => {
 
-    let movieArr = [
-        {
-            "_id": "60e429f57a5d6042284b2faf",
-            "title": "The Black Phone",
-            "released": false
-        },
-        {
-            "_id": "60e41e337a5d6042284b2f4c",
-            "title": "Black Widow",
-            "released": true
-        }
-    ];
+    let movieArr = [];
     let creator;
     let testComponent;
 
-    // test("It matches the snapshot", () => {
-    //     const tree = renderer.create(<Nav />).toJSON();
-    //     expect(tree).toMatchSnapshot();
+    test("It matches the snapshot", () => {
+        const tree = renderer.create(<Nav />).toJSON();
+        expect(tree).toMatchSnapshot();
 
-    // })
+    })
 
     it(`tests the search function`, () => {
         const realUseState = React.useState
 
         jest.spyOn(React, 'useState')
-            .mockImplementationOnce(() => realUseState(false))
+            .mockImplementationOnce(() => realUseState(true))
             .mockImplementationOnce(() => realUseState("black"))
             .mockImplementationOnce(() => realUseState(movieArr))
-
-        const tree = renderer.create(<BrowserRouter><Nav /></BrowserRouter>).toJSON();
-        expect(tree).toMatchSnapshot();
-
-
+        creator = create(<BrowserRouter><Nav /></BrowserRouter >)
+        testComponent = creator.root
     })
 
 })
